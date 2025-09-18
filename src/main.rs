10a0e73f5c7040e8ff233022c231d9dd7d1e1862
xieby1 <xieby1@outlook.com>
2025-09-18 23:26:57 +0800

    clean

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use yake_rust::{get_fn_best, Config, StopWords};
-use poppler;
+use poppler::{PopplerDocument};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -9,15 +9,9 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
-    let file: poppler::PopplerDocument = poppler::PopplerDocument::new_from_file(cli.file, None)
-        .expect("Failed to open PDF file");
+    let file = PopplerDocument::new_from_file(cli.file, None).expect("Failed to open PDF file");
     let mut text = String::new();
-    for page in file.pages() {
-        // This is how to type a loop variable?
-        let page: poppler::PopplerPage = page;
-        text.push_str(&page.get_text().unwrap_or(""));
-    }
-    println!("{}", text);
+    for page in file.pages() { text.push_str(&page.get_text().unwrap_or("")); }
 
     let config = Config {
         ngrams: 3,
@@ -28,12 +22,11 @@ fn main() {
     let ignored = StopWords::custom(
         include_str!("./stopwords.txt").lines().map(ToOwned::to_owned).collect()
     );
-    // TODO: replace `n_keywords` with a ratio of number of words in text
-    // <50->0, 100->4, 1k->6, 10k->8
     let keywords = get_fn_best(
+        // <50->0, 100->4, 1k->6, 10k->8
         |n|{if n<50 {0} else {(f64::log10(n as f64)*2.0) as usize} },
         &text, &ignored, &config
     );
 
-    println!("{:#?}", keywords);
+    for keyword in keywords { println!("{}", keyword.raw); }
 }
